ed453fe7a24191e4541d4cb53f612f6264d1ccc7
davimcruz <davimachadocruz@gmail.com>
2024-09-12 18:50:00 -0300

    Adicionado responsividade para right authors

--- components/main-articles/right-authors.tsx
+++ components/main-articles/right-authors.tsx
@@ -9,7 +9,7 @@ const RightAutors = () => {
          alt="Foto autor 1"
          width={64}
          height={64}
-         className="rounded-full ml-6"
+         className="rounded-full ml-6 max-w-[64px]"
        />
        <div className="flex-col">
          <p className="font-bold ml-4 mt-2">Davi Machado</p>
@@ -21,10 +21,10 @@ const RightAutors = () => {
      <div className="flex py-6">
        <Image
          src="https://img.freepik.com/psd-premium/ilustracao-3d-de-avatar-ou-perfil-humano_23-2150671167.jpg?w=360"
-         alt="Foto autor 1"
+         alt="Foto autor 2"
          width={64}
          height={64}
-         className="rounded-full ml-6"
+         className="rounded-full ml-6 max-w-[64px]"
        />
 
        <div className="flex-col">
@@ -37,26 +37,26 @@ const RightAutors = () => {
      <div className="flex py-4">
        <Image
          src="https://img.freepik.com/psd-gratuitas/ilustracao-3d-de-avatar-ou-perfil-humano_23-2150671132.jpg"
-         alt="Foto autor 1"
+         alt="Foto autor 3"
          width={64}
          height={64}
-         className="rounded-full ml-6"
+         className="rounded-full ml-6 max-w-[64px]"
        />
 
        <div className="flex-col">
          <p className="font-bold ml-4 mt-2">Joana Souza</p>
          <p className="font-normal text-xs ml-4">
-           Diretora de Comunicações  <br /> PUC Goiás
+           Diretora de Comunicações <br /> PUC Goiás
          </p>
        </div>
      </div>
      <div className="flex py-4">
        <Image
          src="https://img.freepik.com/psd-gratuitas/ilustracao-3d-de-avatar-ou-perfil-humano_23-2150671116.jpg?w=360"
-         alt="Foto autor 1"
+         alt="Foto autor 4"
          width={64}
          height={64}
-         className="rounded-full ml-6"
+         className="rounded-full ml-6 max-w-[64px]"
        />
 
        <div className="flex-col">
